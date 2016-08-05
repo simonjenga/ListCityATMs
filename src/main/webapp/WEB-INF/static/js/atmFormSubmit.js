0928ba69213a53e96648534e1ff156076d90de32
Simon N <simtuje@gmail.com>
2016-08-05 08:30:00 +0300

    edited atmFormSubmit js file

--- src/main/webapp/WEB-INF/static/js/atmFormSubmit.js
+++ src/main/webapp/WEB-INF/static/js/atmFormSubmit.js
@@ -7,6 +7,14 @@
 var atmFormSubmit = {
 
 	doAjaxPost : function() {
+		// Put jQuery into no-conflict mode. $j is now an alias to the jQuery function; creating the new alias is optional.
+		var $j = jQuery.noConflict();
 		
+		// get the form values
+		var city = $j('#city').val();
+		
+		$j.ajax({
+   			
+		});
 	}
 }
