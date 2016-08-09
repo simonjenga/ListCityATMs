a1d4c5bc60770391426244c7ab1f47becbb1240c
Simon N <simtuje@gmail.com>
2016-08-09 10:30:00 +0300

    edited ATMControllerTest file

--- src/test/java/com/city/atm/controller/ATMControllerTest.java
+++ src/test/java/com/city/atm/controller/ATMControllerTest.java
@@ -1,11 +1,36 @@
 package com.city.atm.controller;
 
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.city.atm.model.ATM;
+
 /**
  * Test case for {@link ATMController}.
  * 
  * @author Simon Njenga
  * @since 0.1
  */
+@RunWith(value = SpringJUnit4ClassRunner.class)
+@Transactional(transactionManager = "transactionManager")
+@ContextConfiguration(locations = { "classpath:applicationContext.xml" })
+@TestExecutionListeners(value = DependencyInjectionTestExecutionListener.class, inheritListeners = true)
 public class ATMControllerTest {
 
 }
