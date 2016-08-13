91953b1272b796f44dc7683f89589e357ba96ec4
Simon N <simtuje@gmail.com>
2016-08-13 20:20:00 +0300

    edited ATMControllerTest file

--- src/test/java/com/city/atm/controller/ATMControllerTest.java
+++ src/test/java/com/city/atm/controller/ATMControllerTest.java
@@ -105,4 +105,25 @@ public class ATMControllerTest {
         Assert.assertTrue(atm.getType().equals("ING") && !atm.getType().equals("ALBERT_HEIJN"));
         Assert.assertEquals("ROTTERDAM", atm.getAddress().getCity());
     }
+
+    /**
+     * This test should testControllerInCityFour.
+     * 
+     * @throws Exception If something goes wrong
+     */
+    @Test
+    public void testControllerInCityFour() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/listATMsByCity?city=EINDHOVEN")
+            .contentType("application/json;charset=UTF-8").content("{ }"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.content().contentType("application/json;charset=UTF-8"));
+
+        ResponseEntity<List<ATM>> response = this.atmController.listATMsByCity("EINDHOVEN");
+
+        Assert.assertTrue(response != null && response.hasBody() && !response.getBody().isEmpty());
+
+        ATM atm = response.getBody().get(0);
+        Assert.assertTrue(atm.getType().equals("ING") && !atm.getType().equals("ALBERT_HEIJN"));
+        Assert.assertEquals("EINDHOVEN", atm.getAddress().getCity());
+    }
 }
