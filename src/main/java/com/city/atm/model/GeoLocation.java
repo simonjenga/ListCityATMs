47718772142d58661cea69af89fb30a4d711a2ce
Simon N <simtuje@gmail.com>
2016-08-19 09:10:00 +0300

    edited GeoLocation source file

--- src/main/java/com/city/atm/model/GeoLocation.java
+++ src/main/java/com/city/atm/model/GeoLocation.java
@@ -1,5 +1,19 @@
 package com.city.atm.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/**
+ * This is a Domain class that will contain the data (latitude and longitude) representation
+ * for the Geographical Location (GeoLocation) object
+ * 
+ * @author Simon Njenga
+ * @since 0.1
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class GeoLocation {
+
+    private String lat;
+
+    private String lng;
 
 }
