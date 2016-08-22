de486c312641f7bbb42d1b706c6ed6660c1f89ca
Simon N <simtuje@gmail.com>
2016-08-22 20:10:00 +0300

    edited Address source file

--- src/main/java/com/city/atm/model/Address.java
+++ src/main/java/com/city/atm/model/Address.java
@@ -1,5 +1,24 @@
 package com.city.atm.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/**
+ * This is a Domain class that will contain the data representation for the Address object
+ * 
+ * @author Simon Njenga
+ * @since 0.1
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Address {
+
+    private String street;
+
+    private String housenumber;
+
+    private String postalcode;
+
+    private String city;
+
+    private GeoLocation geoLocation;
 
 }
