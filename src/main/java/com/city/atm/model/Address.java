bb4465f12d4ef11294793d30d2bbba5c9c3c394c
Simon N <simtuje@gmail.com>
2016-08-23 20:10:00 +0300

    edited Address source file

--- src/main/java/com/city/atm/model/Address.java
+++ src/main/java/com/city/atm/model/Address.java
@@ -21,4 +21,79 @@ public class Address {
 
     private GeoLocation geoLocation;
 
+    /**
+     * Public default constructor
+     */
+    public Address() {
+    }
+
+    /**
+     * @return the street
+     */
+    public String getStreet() {
+        return this.street;
+    }
+
+    /**
+     * @param street the street to set
+     */
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    /**
+     * @return the housenumber
+     */
+    public String getHousenumber() {
+        return this.housenumber;
+    }
+
+    /**
+     * @param housenumber the housenumber to set
+     */
+    public void setHousenumber(String housenumber) {
+        this.housenumber = housenumber;
+    }
+
+    /**
+     * @return the postalcode
+     */
+    public String getPostalcode() {
+        return this.postalcode;
+    }
+
+    /**
+     * @param postalcode the postalcode to set
+     */
+    public void setPostalcode(String postalcode) {
+        this.postalcode = postalcode;
+    }
+
+    /**
+     * @return the city
+     */
+    public String getCity() {
+        return this.city;
+    }
+
+    /**
+     * @param city the city to set
+     */
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    /**
+     * @return the geoLocation
+     */
+    public GeoLocation getGeoLocation() {
+        return this.geoLocation;
+    }
+
+    /**
+     * @param geoLocation the geoLocation to set
+     */
+    public void setGeoLocation(GeoLocation geoLocation) {
+        this.geoLocation = geoLocation;
+    }
 }
