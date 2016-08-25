551c6cc926333ebb34ce0e714fc2b7e3d8c8a4dc
Simon N <simtuje@gmail.com>
2016-08-25 20:10:00 +0300

    edited ATM source file

--- src/main/java/com/city/atm/model/ATM.java
+++ src/main/java/com/city/atm/model/ATM.java
@@ -1,5 +1,20 @@
 package com.city.atm.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/**
+ * This is a Domain class that will contain the data representation for the ATM object
+ * 
+ * @author Simon Njenga
+ * @since 0.1
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ATM {
+
+    private Address address;
+
+    private Long distance;
+
+    private String type;
 
 }
