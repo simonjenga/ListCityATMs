17fc8a93d32cc2099d17d1cd1a6722d097e92c05
Simon N <simtuje@gmail.com>
2016-08-26 20:10:00 +0300

    edited ATM source file

--- src/main/java/com/city/atm/model/ATM.java
+++ src/main/java/com/city/atm/model/ATM.java
@@ -17,4 +17,51 @@ public class ATM {
 
     private String type;
 
+    /**
+     * Public default constructor
+     */
+    public ATM() {
+    }
+
+    /**
+     * @return the address
+     */
+    public Address getAddress() {
+        return this.address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    /**
+     * @return the distance
+     */
+    public Long getDistance() {
+        return this.distance;
+    }
+
+    /**
+     * @param distance the distance to set
+     */
+    public void setDistance(Long distance) {
+        this.distance = distance;
+    }
+
+    /**
+     * @return the type
+     */
+    public String getType() {
+        return this.type;
+    }
+
+    /**
+     * @param type the type to set
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
 }
