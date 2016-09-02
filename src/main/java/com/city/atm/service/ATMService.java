03cac486f6a5e5f115b776730310d55591fed902
Simon N <simtuje@gmail.com>
2016-09-02 20:10:00 +0300

    added ATMService source file

--- src/main/java/com/city/atm/service/ATMService.java
+++ src/main/java/com/city/atm/service/ATMService.java
@@ -1,5 +1,17 @@
 package com.city.atm.service;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.city.atm.component.ATMComponent;
+import com.city.atm.model.ATM;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+
+@Service
 public class ATMService {
 
 }
