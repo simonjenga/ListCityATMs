7144f04aae2941f074cf65bd33e50d892faf4f92
Simon N <simtuje@gmail.com>
2016-09-05 20:10:00 +0300

    added MyProcessor source file

--- src/main/java/com/city/atm/configuration/MyProcessor.java
+++ src/main/java/com/city/atm/configuration/MyProcessor.java
@@ -1,14 +1,5 @@
 package com.city.atm.configuration;
 
-import org.apache.camel.Exchange;
-import org.apache.camel.Message;
-
 public class MyProcessor {
 
-    public void process(Exchange exchange) {
-        System.out.println("process");
-        Message in = exchange.getIn();
-        byte[] file = in.getBody(byte[].class);
-        System.out.println("bytes found:" + file.length);
-    }
 }
