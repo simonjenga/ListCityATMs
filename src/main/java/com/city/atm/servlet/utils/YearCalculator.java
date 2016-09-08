1de143eb0a8bb27dae65176b16c6f75051186548
Simon N <simtuje@gmail.com>
2016-09-08 20:10:00 +0300

    added YearCalculator source file

---
+++ src/main/java/com/city/atm/servlet/utils/YearCalculator.java
@@ -0,0 +1,12 @@
+package com.city.atm.servlet.utils;
+
+/**
+ * This subclass of {@code HttpServlet} creates an HTTP servlet that computes the current year
+ * and the output is printed on the footer of every JSP page.
+ * 
+ * @author Simon Njenga
+ * @since 0.1
+ */
+public class YearCalculator {
+
+}
