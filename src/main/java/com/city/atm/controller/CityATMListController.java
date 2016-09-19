aaa7e9a20b184396f5fdd47d7bab922fc7960454
Simon N <simtuje@gmail.com>
2016-09-19 20:10:00 +0300

    edited CityATMListController source file

--- src/main/java/com/city/atm/controller/CityATMListController.java
+++ src/main/java/com/city/atm/controller/CityATMListController.java
@@ -1,5 +1,21 @@
 package com.city.atm.controller;
 
+import java.security.Principal;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
 public class CityATMListController {
 
 }
