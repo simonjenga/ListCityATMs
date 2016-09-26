e2f57a5c02d88b1cfd30f938a517dcc2773ecb73
Simon N <simtuje@gmail.com>
2016-09-26 09:10:00 +0300

    added missing import statements

--- src/main/java/com/city/atm/servlet/utils/YearCalculator.java
+++ src/main/java/com/city/atm/servlet/utils/YearCalculator.java
@@ -1,5 +1,14 @@
 package com.city.atm.servlet.utils;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Calendar;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * This subclass of {@code HttpServlet} creates an HTTP servlet that computes the current year
  * and the output is printed on the footer of every JSP page.
