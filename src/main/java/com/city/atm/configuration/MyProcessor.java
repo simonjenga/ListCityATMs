e6514707cb9429023e922f6ad6a491109625ba77
Simon N <simtuje@gmail.com>
2017-06-08 14:45:00 +0300

    provided the documentation for MyProcessor class

--- src/main/java/com/city/atm/configuration/MyProcessor.java
+++ src/main/java/com/city/atm/configuration/MyProcessor.java
@@ -4,7 +4,17 @@ import org.apache.camel.Exchange;
 import org.apache.camel.Message;
 
 /**
- * Class description to be done later!
+ * This class uses an Apache Camel Processor {@link org.apache.camel.Processor} to adjust or
+ * change a message from the processor.
+ *
+ * Please visit here for more details: 
+ * http://camel.apache.org/using-getin-or-getout-methods-on-exchange.html
+ 
+ * A processor is used to implement the <b>Event Driven Consumer</b> and <b>Message Translator</b> patterns
+ * and to process message exchanges.
+ *
+ * http://camel.apache.org/event-driven-consumer.html
+ * http://camel.apache.org/message-translator.html
  *
  * @author Simon Njenga
  * @since 0.1
