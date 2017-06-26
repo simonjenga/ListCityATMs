46bc2236a91540a489f267e313317a3d66083fdd
Simon N <simtuje@gmail.com>
2017-06-26 13:45:00 +0300

    provided the documentation for atm service class

--- src/main/java/com/city/atm/service/ATMService.java
+++ src/main/java/com/city/atm/service/ATMService.java
@@ -12,7 +12,7 @@ import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 
 /**
- * Class description to be done later!
+ * Service implementation class that provides a service for listing <code>ATM</code> machines.
  *
  * @author Simon Njenga
  * @since 0.1
@@ -23,11 +23,26 @@ public class ATMService {
     @Autowired
     private ATMComponent atmComponent;
 
+    /**
+     * Retrieves a list of all <code>ATM</code> objects from the <code>ATMComponent</code> class.
+     * @throws JsonParseException If there is an error parsing the JSON response or the response is not a valid JSON
+     * @throws JsonMappingException If there is a problem in accessing suitable deserializer; including that of not finding any serializer
+     * @throws IOException If there are some any other problems inside
+     * @return a <code>List</code> of <code>ATM</code> objects
+     */
     public List<ATM> listATMs() throws JsonParseException,
             JsonMappingException, IOException {
         return this.atmComponent.listATMs();
     }
 
+    /**
+     * Retrieves a list of all <code>ATM</code> objects from the <code>ATMComponent</code> class.
+     * @param city the city name to perform a look up of atm's machines
+     * @throws JsonParseException If there is an error parsing the JSON response or the response is not a valid JSON
+     * @throws JsonMappingException If there is a problem in accessing suitable deserializer; including that of not finding any serializer
+     * @throws IOException If there are some any other problems inside
+     * @return a <code>List</code> of <code>ATM</code> objects
+     */
     public List<ATM> listATMsByCity(String city) throws JsonParseException,
             JsonMappingException, IOException {
         return this.atmComponent.listATMsByCity(city);
