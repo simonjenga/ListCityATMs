27a1954d921c3628a6b8025d70b616b58c0f5bb3
Simon N <simtuje@gmail.com>
2017-06-27 15:15:00 +0300

    provided the documentation for atm component class

--- src/main/java/com/city/atm/component/ATMComponent.java
+++ src/main/java/com/city/atm/component/ATMComponent.java
@@ -16,17 +16,29 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
- * Class description to be done later!
+ * Apache Camel offers a Java based Domain Specific Language (DSL) using the fluent builder style.
+ *
+ * The Java DSL is made available by extending the <code>RouteBuilder</code> class, and implementing
+ * the {@link #configure()} method. In our subclass here, we extend the <code>RouteBuilder</code> class
+ * and then override it's {@link #configure()} method where we define our routing rules.
  *
  * @author Simon Njenga
  * @since 0.1
  */
-@Component
+@Component(value = "atmComponent")
 public class ATMComponent extends RouteBuilder {
 
-    // URL for the external service API
+
+    /** This is the URL for the external service API */
     private static final String URL = "https://www.ing.nl/api/locator/atms/";
 
+    /**
+     * Retrieves a list of all <code>ATM</code> objects from the <code>ATMComponent</code> class.
+     * @throws JsonParseException If there is an error parsing the JSON response or the response is not a valid JSON
+     * @throws JsonMappingException If there is a problem in accessing suitable deserializer; including that of not finding any serializer
+     * @throws IOException If there are some any other problems inside
+     * @return a <code>List</code> of <code>ATM</code> objects
+     */
     public List<ATM> listATMs() throws JsonParseException,
             JsonMappingException, IOException {
         RestTemplate restTemplate = new RestTemplate();
@@ -39,6 +51,14 @@ public class ATMComponent extends RouteBuilder {
         return Arrays.asList(atms);
     }
 
+    /**
+     * Retrieves a list of all <code>ATM</code> objects from the <code>ATMComponent</code> class.
+     * @param city the city name to perform a look up of atm's machines
+     * @throws JsonParseException If there is an error parsing the JSON response or the response is not a valid JSON
+     * @throws JsonMappingException If there is a problem in accessing suitable deserializer; including that of not finding any serializer
+     * @throws IOException If there are some any other problems inside
+     * @return a <code>List</code> of <code>ATM</code> objects
+     */
     public List<ATM> listATMsByCity(String city) throws JsonParseException,
             JsonMappingException, IOException {
         RestTemplate restTemplate = new RestTemplate();
